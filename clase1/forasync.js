2039b1374b6db8e7999aae19529d7ca805e55250
andy <arteysoft@gmail.com>
2021-10-15 20:36:17 -0300

    ejercicio y recreo

--- clase1/forasync.js
+++ clase1/forasync.js
@@ -8,10 +8,10 @@ for (let x = 1; x <= 100; x++) {
 
 asyncforloop(100, (nroIteracion, next, abort) => {
     console.log(nroIteracion)
-    if (nroIteracion===55) {
-        abort(new Error('guarda que el 55 es mufa!!!'))
-        return
-    }
+    //if (nroIteracion === 55) {
+    //    abort(new Error('guarda que el 55 es mufa!!!'))
+    //    return
+    //}
     next()
 }, err => {
     console.log(err)
