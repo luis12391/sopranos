97ceba382b088e17d2a062036e868dc2fcf19007
Luis Sanchez <luise12391@gmail.com>
2021-10-15 21:10:48 -0300

    Added first task

---
+++ clase1/consigna1.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+var asyncforloop = require('@arteysoft/asyncforloop');
+
+function start(){
+    var numbers= "";
+    asyncforloop(100, (nroIteracion, next, abort) => {
+        try {
+            numbers+= `Estoy en la interacion ${nroIteracion}\n`;
+            next()            
+        } catch (error) {
+            abort(new Error('Se ha proiducido un error!!!'))
+        }
+
+    }, err => {
+        if (err)
+            console.log(err)
+        else
+            writeAFile(numbers);
+
+        console.log('finalizo el asyncforloop')
+    })
+};
+
+function writeAFile (numbers){
+    fs.writeFile('./cosignaLuis.txt', `${numbers}`, error => {
+        if (error)
+          console.log(error);
+        else
+          console.log('El archivo fue creado');
+      });
+};
+
+start();
+  
